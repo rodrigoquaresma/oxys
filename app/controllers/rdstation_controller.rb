348d0bd8e9e646a53472e68b66ed08aeab5c59d5
quaresma <rodrigoquaresma@gmail.com>
2014-06-28 22:28:51 -0300

    clean fase 1

--- app/controllers/rdstation_controller.rb
+++
@@ -1,8 +0,0 @@
-#coding: utf-8
-class RdstationController < ApplicationController
-  
-  def lp_responsive
-    render "rdstation/lp_responsive"
-  end
-
-end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Oxys::Application.config.session_store :cookie_store, key: '_oxys_session'
+Rails.application.config.session_store :cookie_store, key: '_oxys2_session'
