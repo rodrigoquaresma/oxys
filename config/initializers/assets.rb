de4047842126d243af30817f50583f2598838ca7
quaresma <rodrigoquaresma@gmail.com>
2014-06-29 00:41:16 -0300

    fix scss

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,4 +7,4 @@ Rails.application.config.assets.version = '1.1'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.initialize_on_precompile = false
+# Rails.application.config.assets.initialize_on_precompile = false
