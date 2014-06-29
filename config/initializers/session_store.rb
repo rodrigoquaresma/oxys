70597ed8403adea83625698da397e9e3d265b7d3
quaresma <rodrigoquaresma@gmail.com>
2014-06-28 23:58:51 -0300

    fix

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_oxys2_session'
+Rails.application.config.session_store :cookie_store, key: '_oxys_session'
